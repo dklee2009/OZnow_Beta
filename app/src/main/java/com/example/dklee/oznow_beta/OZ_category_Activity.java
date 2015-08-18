7c3af937bfe38e37c371a10f2578a0ad9f12f81d
DKLEE <dklee1990@naver.com>
2015-08-18 17:00:09 +0900

    카테고리 다이얼로그 생성

--- app/src/main/java/com/example/dklee/oznow_beta/OZ_category_Activity.java
+++ app/src/main/java/com/example/dklee/oznow_beta/OZ_category_Activity.java
@@ -2,9 +2,11 @@ package com.example.dklee.oznow_beta;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -16,6 +18,29 @@ public class OZ_category_Activity extends AppCompatActivity {
 
         setContentView(R.layout.oz_write_category);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+
+        Button ctg_btn_1 = (Button)findViewById(R.id.ctg_btn1);
+        Button ctg_btn_2 = (Button)findViewById(R.id.ctg_btn2);
+        Button ctg_btn_3 = (Button)findViewById(R.id.ctg_btn3);
+        Button ctg_btn_4 = (Button)findViewById(R.id.ctg_btn4);
+        Button ctg_btn_5 = (Button)findViewById(R.id.ctg_btn5);
+        Button ctg_btn_6 = (Button)findViewById(R.id.ctg_btn6);
+        Button ctg_btn_7 = (Button)findViewById(R.id.ctg_btn7);
+        Button ctg_btn_8 = (Button)findViewById(R.id.ctg_btn8);
+
+        TextView ctg_txt1 = (TextView)findViewById(R.id.ctg_txt1);
+        TextView ctg_txt2 = (TextView)findViewById(R.id.ctg_txt2);
+        TextView ctg_txt3 = (TextView)findViewById(R.id.ctg_txt3);
+        TextView ctg_txt4 = (TextView)findViewById(R.id.ctg_txt4);
+        TextView ctg_txt5 = (TextView)findViewById(R.id.ctg_txt5);
+        TextView ctg_txt6 = (TextView)findViewById(R.id.ctg_txt6);
+        TextView ctg_txt7 = (TextView)findViewById(R.id.ctg_txt7);
+        TextView ctg_txt8 = (TextView)findViewById(R.id.ctg_txt8);
+
+        OZ_category_dialog ctg_dialog;
+
+        ctg_dialog = new OZ_category_dialog(OZ_category_Activity.this);
+
 
     }
 
---
+++ app/src/main/java/com/example/dklee/oznow_beta/OZ_category_dialog.java
@@ -0,0 +1,52 @@
+package com.example.dklee.oznow_beta;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by DKLEE on 2015-08-18.
+ */
+public class OZ_category_dialog extends Dialog implements View.OnTouchListener {
+
+    private EditText dialog_edittxt;
+    private Button dialog_ok , dialog_cancel;
+    private String dialog_category;
+
+    public OZ_category_dialog(Context context){
+        super(context);
+    }
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.oz_dialog_category);
+
+        dialog_edittxt = (EditText)findViewById(R.id.dialog_edittxt);
+        dialog_ok = (Button)findViewById(R.id.dialog_ok);
+        dialog_cancel = (Button)findViewById(R.id.dialog_cancel);
+
+        dialog_ok.setOnClickListener(this);
+        dialog_cancel.setOnClickListener(this);
+    }
+
+    public String getcategoryName(){
+        return dialog_category;
+    }
+
+    public boolean onClick(View v,MotionEvent event){
+
+        if( v== dialog_ok){
+            dialog_category = dialog_edittxt.getText().toString();
+            dismiss();
+        }
+        else if(v==dialog_cancel)
+            cancel();
+
+        return false;
+
+    }
+}
